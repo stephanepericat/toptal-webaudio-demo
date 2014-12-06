b47f3373b15a22c48e505ce074e1788f3387c9d7
Stéphane P. Péricat <contact@pericat.net>
2014-12-06 15:00:34 +0100

    midi devices

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,48 @@
 angular
-    .module('Synth', [])
-    .controller('SynthCtrl', ['$scope', function($scope) {
+    .module('Synth', ['WebAudio', 'WebMIDI'])
+    .controller('SynthCtrl', ['$scope', 'Devices', function($scope, Devices) {
+        $scope.devices = [];
 
+        Devices
+            .connect()
+            .then(function(access) {
+
+                if('function' === typeof access.inputs) {
+                    // deprecated
+                    // $scope.devices = access.inputs();
+                    // $($scope.devices).each(function(idx, device) {
+                    //     device.onmidimessage = function(e) {
+                    //         if($scope.activeDevice && $scope.activeDevice.name === this.name) {
+                    //             console.log('active device');
+                    //             console.log('MIDI MESSAGE', e);
+                    //         }
+                    //     };
+                    // });
+                } else {
+                    var inputs = access.inputs.values(),
+                    input, device;
+
+                    for (input = inputs.next(); input && !input.done; input = inputs.next()) {
+                        device = input.value;
+
+                        device.onmidimessage = function(e) {
+                            if($scope.activeDevice && $scope.activeDevice.name === this.name) {
+                                console.log('active device');
+                                console.log('MIDI MESSAGE', e);
+                            }
+                        };
+
+                        $scope.devices.push(device);
+                    }
+                }
+            });
+
+        $scope.$watch('activeDevice', function(n, o) {
+            if(n) {
+                console.log(n);
+                console.log('active device: %s %s', n.manufacturer, n.name);
+            }
+        });
     }]);
 
 angular
--- app/js/midi.js
+++ app/js/midi.js
@@ -1,2 +1,28 @@
 angular
-    .module('WebMidi', []);
+    .module('WebMIDI', [])
+    .factory('Devices', ['$window', '$q', function($window, $q) {
+        function _test() {
+            return ($window.navigator && $window.navigator.requestMIDIAccess) ? true : false;
+        }
+
+        function _connect() {
+            var d = $q.defer(),
+            p = d.promise
+            a = null;
+
+            if(_test()) {
+                $window
+                    .navigator
+                    .requestMIDIAccess()
+                    .then(d.resolve, d.reject);
+            } else {
+                d.reject(new Error('No Web MIDI support'));
+            }
+
+            return p;
+        }
+
+        return {
+            connect: _connect
+        };
+    }]);
