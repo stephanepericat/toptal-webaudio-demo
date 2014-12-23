46aadeee8bb828b4f9e79c91aa2e33133f04d881
Stéphane P. Péricat <contact@pericat.net>
2014-12-23 16:27:40 +0100

    started article

--- server.js
+++ server.js
@@ -2,5 +2,6 @@ var express = require('express'),
     app = express();
 
 app.use('/', express.static(__dirname + '/app'));
+app.use('/article', express.static(__dirname + '/article'));
 
 app.listen(process.env.PORT || 3000);
